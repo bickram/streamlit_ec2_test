1b29b965dbea174aceaf1cf6e8edae9847b6ff1a
Dipesh Thapa <dipesh.thapa@gmail.com>
2023-10-07 17:17:02 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+headerSelection = st.container()
+mainSection = st.container()
+LeftNav = st.sidebar
+
+with headerSelection:
+    st.title("Streamlit")
+    st.markdown("This is a demo of Streamlit")
+
+with mainSection:
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.header("Left Column")
+        st.markdown("Left Column")
+        textValue = st.text_input("Enter text", "")
+        st.write(textValue)
+
+    with right_col:
+        st.header("Right Column")
+        st.markdown("RIght Column")
+    
+
+with LeftNav:
+    st.button("menu1", on_click=lambda: st.success("Menu1"))
+    st.button("menu2", on_click=lambda: st.success("Menu2"))

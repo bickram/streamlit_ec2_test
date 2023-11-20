640c383bbf1a0302a9763a902ef88990143490b6
Dipesh Thapa <dipesh.thapa@gmail.com>
2023-11-19 21:15:28 -0500

    Title detail revised agains

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ LeftNav = st.sidebar
 
 with headerSelection:
     st.title("Streamlit")
-    st.markdown("This is a demo of Streamlit - TEST-Repeated2")
+    st.markdown("This is a demo of Streamlit - TEST-Repeated3")
 
 with mainSection:
     left_col, right_col = st.columns(2)
